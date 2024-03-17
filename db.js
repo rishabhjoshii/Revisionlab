13c9e75645bf7381f6cda3e058f10af43d6f08b2
rishabhjoshii <rishabhjoshi@gmail.com>
2024-03-17 20:24:30 +0530

    commit1

---
+++ db.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    username : {
+        type: String,
+        required: true,
+        min : 4,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique : true,
+    },
+    password : {
+        type: String,
+        required: true,
+        min: 6
+    }
+})
+
+const dsaSchema = new Schema({
+    title: String,
+    url: String,
+    tags: String,
+    author:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+const Dsa = mongoose.model('Dsa',dsaSchema);
+const User = mongoose.model('DsaRevisionUser',userSchema);
+
+module.exports = {Dsa,User};
---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const zod = require('zod');
+const {Dsa, User} = require('./db');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const userSchema = zod.object({
+    username : zod.string(),
+    email : zod.string().email(),
+    password : zod.string()
+})
+
+const jwtSecretKey = "hggfdgffxfdgrs77888";
+const salt = bcrypt.genSaltSync(10);
+
+
+mongoose.connect(process.env.DATABASE_URL)
+    .then(()=> {
+        console.log("connected to mongodb");
+    })
+    .catch((err) => {
+        console.log("can't connect to mongodb",err);
+    })
+
+app.post('/signup', async function(req, res, next){
+    try{
+        const {username,email,password} = req.body;
+        const result = await userSchema.safeParse(req.body);
+        if(!result.success){
+            return res.status(400).json({msg: "invalid credentials"});
+        }
+    
+        const newUser = await new User({
+            username,
+            email,
+            password :bcrypt.hashSync(password,salt),
+        })
+        await newUser.save();
+    
+        return res.json({msg:"signup successful"});
+    }
+    catch(err){
+        console.log("error while signing up",err);
+        return res.status(400).json({msg:"invalid credentials",err});
+    }
+})
+
+app.post('/login', async function(req, res, next){
+    try{
+        const {username,email,password} = req.body;
+        const result = await userSchema.safeParse(req.body);
+        if(!result.success){
+            return res.status(400).json({msg: "invalid credentials"});
+        }
+
+        const user = await User.findOne({username,email});
+        if(!user) return res.status(401).json({msg: "user not found"});
+
+        const passwordVerify = bcrypt.compareSync(password,user.password);
+
+        if(!passwordVerify) return res.status(401).json({msg: "Incorrect password"});
+
+        const token = jwt.sign({username,email},process.env.JWT_SECRET_KEY);
+        console.log("token is here:",token);
+
+        return res.json({msg: "login successful",token:token});
+    }
+    catch(err){
+        console.log("error while login", err);
+        return res.status(400).json({msg:"error while login",err});
+    }
+} )
+
+
+
+app.listen(3000);
